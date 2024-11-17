e042af6050901f0c22ca550c651cfb8b43e82a92
Julian Jekov <drmsz@abv.bg>
2024-11-17 11:42:52 +0200

    Backend init

---
+++ server/src/main/java/com/example/unique/wear/UniqueWearApplication.java
@@ -0,0 +1,13 @@
+package com.example.unique.wear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueWearApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/unique/wear/UniqueWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unique.wear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
